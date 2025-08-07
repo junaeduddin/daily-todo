258908ff525e51b1c8f6140ec1d0fb2299b0df15
junaeduddin <junaed.uddin12@gmail.com>
2025-08-07 08:28:50 +0600

    Add initial HTML, CSS, and JavaScript for a basic interactive webpage

---
+++ 250807_practice/script.js
@@ -0,0 +1,15 @@
+ function myParagraph() {
+        let para = document.getElementById("text")
+        para.style.color = "purple";
+    }
+
+    function myParagraph2() {
+        let para2 = document.getElementById("text")
+        para2.style.color = "black";
+    }
+
+    function myParagraph3() {
+        let para3 = document.getElementById("container")
+        para3.style.background = "green";
+    }
+
